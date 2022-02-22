d794924eecf122c612de5292b97f4c29a2f7f68d
정재운 <jaeun101501@gmail.com>
2022-02-22 22:51:36 +0900

    png 수정

--- main.lua
+++ main.lua
@@ -12,6 +12,7 @@ local widget = require "widget"
 local composer = require "composer"
 local physics = require "physics"
 physics.start()
+physics.setDrawMode("hybrid")
 
 -- event listeners for tab buttons:
 local function onFirstView( event )
