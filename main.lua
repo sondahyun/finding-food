cc34d70d646cfa8d4e23c78f7353b3872a7191af
kimseoyoungk <99073581+kimseoyoungk@users.noreply.github.com>
2022-02-23 20:14:06 +0900

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,23 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+--test
+--test1
+-- show default status bar (iOS)
+display.setStatusBar( display.DefaultStatusBar )
+
+-- include Corona's "widget" library
+local widget = require "widget"
+local composer = require "composer"
+local physics = require "physics"
+physics.start()
+physics.setDrawMode("hybrid")
+
+-- event listeners for tab buttons:
+local function onFirstView( event )
+	composer.gotoScene( "View01_cat2" )
+end
+
+onFirstView()	-- invoke first tab button's onPress event manually
