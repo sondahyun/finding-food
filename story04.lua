d2b020ba98f0e8e263c129f3e5ddc459204fcec8
kimseoyoungk <99073581+kimseoyoungk@users.noreply.github.com>
2022-02-24 13:23:02 +0900

    Update story04.lua

--- story04.lua
+++ story04.lua
@@ -38,11 +38,11 @@ function scene:create( event )
 
 	local speakerImg = display.newRect(section.x - 360, section.y - 400, 300, 300)
 
-	local speaker = display.newText("더미 텍스트", section.x-250, section.y-75)
+	local speaker = display.newText("더미 텍스트", section.x-400, section.y-75)
 	speaker.size = 80
 	speaker:setFillColor(0)
 
-		local script = display.newText("더미 텍스트입니다.", section.x+220, section.y+30, display.contentWidth, 120)
+		local script = display.newText("더미 텍스트입니다.", section.x+100, section.y+30, display.contentWidth, 120)
 	script.width = display.contentWidth*0.5
 	script.size = 55
 	script:setFillColor(0)
