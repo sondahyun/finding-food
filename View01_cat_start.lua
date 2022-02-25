854c630e139f403a9d851e5cce76e0eb08010472
kimseoyoungk <99073581+kimseoyoungk@users.noreply.github.com>
2022-02-25 19:13:29 +0900

    Create View01_cat_start.lua

---
+++ View01_cat_start.lua
@@ -0,0 +1,79 @@
+-----------------------------------------------------------------------------------------
+--
+-- view1.lua
+--
+-----------------------------------------------------------------------------------------
+
+local composer = require( "composer" )
+local scene = composer.newScene()
+
+function scene:create( event )
+	local sceneGroup = self.view
+	
+	local explosionSound = audio.loadSound( "Content/PNG/script/계곡물 흐르는 소리.mp3" )
+	audio.play( explosionSound )
+
+	local background = display.newImageRect("Content/PNG/타이틀/미니게임타이틀_물고기.png", display.contentWidth, display.contentHeight)
+	background.x, background.y = display.contentWidth/2, display.contentHeight/2
+
+
+	sceneGroup:insert(background)
+
+	local function tap(event)
+        --audio.pause(myBday)
+        composer.removeScene("View01_cat_start")
+		composer.gotoScene("View01_cat2")
+	end
+
+	background:addEventListener("tap",tap)
+
+end
+
+function scene:show( event )
+	local sceneGroup = self.view
+	local phase = event.phase
+	
+	if phase == "will" then
+		-- Called when the scene is still off screen and is about to move on screen
+	elseif phase == "did" then
+		-- Called when the scene is now on screen
+		-- 
+		-- INSERT code here to make the scene come alive
+		-- e.g. start timers, begin animation, play audio, etc.
+	end	
+end
+
+function scene:hide( event )
+	local sceneGroup = self.view
+	local phase = event.phase
+	
+	if event.phase == "will" then
+		-- Called when the scene is on screen and is about to move off screen
+		--
+		-- INSERT code here to pause the scene
+		-- e.g. stop timers, stop animation, unload sounds, etc.)
+	elseif phase == "did" then
+		-- Called when the scene is now off screen
+	end
+end
+
+function scene:destroy( event )
+	local sceneGroup = self.view
+	
+	-- Called prior to the removal of scene's "view" (sceneGroup)
+	-- 
+	-- INSERT code here to cleanup the scene
+	-- e.g. remove display objects, remove touch listeners, save state, etc.
+end
+
+---------------------------------------------------------------------------------
+
+-- Listener setup
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+
+-----------------------------------------------------------------------------------------
+
+return scene
