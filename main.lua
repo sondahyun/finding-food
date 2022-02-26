d7702ee8da105664bb16f0159e809167d3472d6b
정재운 <jaeun101501@gmail.com>
2022-02-26 21:27:51 +0900

    수정

--- main.lua
+++ main.lua
@@ -13,7 +13,7 @@ local physics = require "physics"
 physics.start()
 -- event listeners for tab buttons:
 local function onFirstView( event )
-	composer.gotoScene( "diaryviewTest" )
+	composer.gotoScene( "View01_bear" )
 end
 
 onFirstView()	-- invoke first tab button's onPress event manually
